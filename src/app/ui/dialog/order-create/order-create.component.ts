53dc9e3b9b2315f82ed4af6a7da907627918103e
Vadim Lisovyi <vadim.lisovyi@gmail.com>
2019-08-19 16:35:07 +0300

    [BOODMO-16973] fix create-order date validation

--- src/app/ui/dialog/order-create/order-create.component.ts
+++ src/app/ui/dialog/order-create/order-create.component.ts
@@ -6,8 +6,8 @@ import {
   NgbModal,
 } from '@ng-bootstrap/ng-bootstrap';
 import {
-  FormBuilder,
-  FormGroup, Validators,
+  FormBuilder, FormControl,
+  FormGroup, Validators
 } from '@angular/forms';
 import { ModelService } from '../../../model/model.service';
 import { DatePipe } from '@angular/common';
@@ -30,15 +30,18 @@ export class OrderCreateComponent implements OnInit {
     public model: ModelService,
     private datePipe: DatePipe,
   ) {
+    this.minDate = this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm');
   }
 
   ngOnInit() {
-    this.minDate = this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm');
 
     this.form = this.fb.group({
       orderId: [this.model.currentOrder.id, []],
       customerId: [this.model.customers.first().id, Validators.required],
-      date: [this.minDate, Validators.required],
+      date: [this.minDate, [
+        Validators.required,
+        this.validateDate
+      ]],
       orderItem: this.fb.group({
         sellerId: [this.model.sellers.first().id, Validators.required],
         itemId: [this.model.items.first().id, Validators.required],
@@ -55,7 +58,27 @@ export class OrderCreateComponent implements OnInit {
           Validators.min(0),
         ]],
       })
-    });
+    }, {validator: this.checkDate()});
+  }
+
+  checkDate() {
+    return (group: FormGroup): {[key: string]: any} => {
+      const d = group.controls['date'];
+      const min = new Date(this.minDate);
+      const test = new Date(d.value);
+      return (min > test) ? {
+        checkDate: `Date should be greater than ${this.datePipe.transform(this.minDate, 'yyyy-MM-dd HH:gsmm')}`
+      } : {};
+    };
+  }
+  validateDate = (c: FormControl) => {
+    const min = new Date(this.minDate);
+    const test = new Date(c.value);
+    return (min > test) ? {
+      checkDate: {
+        valid: false
+      }
+    } : null;
   }
 
   open(content) {
