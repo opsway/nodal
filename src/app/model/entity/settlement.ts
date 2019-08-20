ed3d8d0adee4b024be9dc76bcdf64823e83d1436
Ilyar <ilyar.software@gmail.com>
2019-08-20 09:49:53 +0300

    fix calc gateway settlement

--- src/app/model/entity/settlement.ts
+++ src/app/model/entity/settlement.ts
@@ -7,7 +7,12 @@ import { Collection } from '../collection';
 export class Settlement implements Entity {
   id: string;
   fee = 0;
-  amount = 0;
+  total = 0;
+  references: string[] = [];
+  totalPayment = 0;
+  countPayment = 0;
+  totalRefund = 0;
+  countRefund = 0;
 
   constructor(
     public gateway: string,
@@ -17,36 +22,22 @@ export class Settlement implements Entity {
     this.id = Util.uuid('ST_GW_');
   }
 
-  get total(): number {
-    return this.amount + this.fee;
-  }
-
-  get references(): string {
-    return this.paymentCollection.map(entity => entity.id).join(', ');
-  }
-
-  get totalPayment(): number {
-    return this.paymentCollection.reduce((entity, acc) => acc + entity.total, 0);
-  }
-
-  get countPayment(): number {
-    return this.paymentCollection.count();
-  }
-
-  get totalRefund(): number {
-    return this.paymentCollection.reduce((entity, acc) => acc + entity.totalRefund, 0);
-  }
-
-  get countRefund(): number {
-    return this.paymentCollection.reduce((entity, acc) => acc + entity.countRefund, 0);
+  get amount(): number {
+    return this.total + this.fee;
   }
 
   capture(payment: Payment): Settlement {
     const feeGateway = Math.floor((Model.paymentGatewayFee / 100) * payment.totalSettlement);
-    this.amount += payment.totalSettlement - feeGateway;
+    this.total += payment.totalSettlement - feeGateway;
     this.fee += feeGateway;
     payment.capture(feeGateway, this.createdAt);
     this.paymentCollection.add(payment);
+    this.references = this.paymentCollection.map(entity => entity.id);
+    this.totalPayment = this.paymentCollection.reduce((entity, acc) => acc + entity.total, 0);
+    this.countPayment = this.paymentCollection.count();
+    this.totalRefund = this.paymentCollection.reduce((entity, acc) => acc + entity.totalRefund, 0);
+    this.countRefund = this.paymentCollection.reduce((entity, acc) => acc + entity.countRefund, 0);
+
     return this;
   }
 }
