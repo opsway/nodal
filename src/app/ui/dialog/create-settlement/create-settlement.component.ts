57f656d9740e29a6e5ecc1c7e0da91672a2da0f3
Vadim Lisovyi <vadim.lisovyi@gmail.com>
2019-08-20 11:35:14 +0300

    [BOODMO-16973] fix dateFormat for create-settlement

--- src/app/ui/dialog/create-settlement/create-settlement.component.ts
+++ src/app/ui/dialog/create-settlement/create-settlement.component.ts
@@ -6,6 +6,7 @@ import {
   DatePipe,
 } from '@angular/common';
 import { ModelService } from '../../../model/model.service';
+import { VirtualDateService } from '../../../util/virtual-date.service';
 
 @Component({
   selector: 'app-create-settlement',
@@ -23,12 +24,13 @@ export class CreateSettlementComponent implements OnInit {
   constructor(
     private datePipe: DatePipe,
     public model: ModelService,
+    private dateService: VirtualDateService,
   ) {
   }
 
   ngOnInit() {
     this.paymentGateway = this.model.paymentMethods[0];
-    this.settlementDate = this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm');
+    this.settlementDate = this.dateService.getValue();
     // TODO this.dateMin = this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm');
     // TODO this.dateMax = this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm');
   }
