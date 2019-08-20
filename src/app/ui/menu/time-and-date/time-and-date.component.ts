d9543e9d8db84211d38ba43b9663a3feff7e6008
Vadim Lisovyi <vadim.lisovyi@gmail.com>
2019-08-20 13:38:44 +0300

    [BOODMO-16973] clean code

--- src/app/ui/menu/time-and-date/time-and-date.component.ts
+++ src/app/ui/menu/time-and-date/time-and-date.component.ts
@@ -23,7 +23,7 @@ export class TimeAndDateComponent implements OnInit {
     this.updateVirtualDate();
   }
 
-  update = () => {
+  update() {
     const date = this.now.split('T');
     this.nowDate = date[0];
     this.nowTime = date[1];
@@ -32,8 +32,6 @@ export class TimeAndDateComponent implements OnInit {
   setVirtualDate = () => this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm:ss');
 
   updateVirtualDate = () => {
-    console.log(`${this.nowDate}T${this.nowTime}`);
-    console.log(this.now);
     const diff = Date.now() - (new Date(`${this.nowDate}T${this.nowTime}`)).getTime();
     localStorage.setItem('virtual_date_diff', `${diff || 0}`);
   }
