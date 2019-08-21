8f70105799aa0cc2bc0c0f64dd0121ebc9bab85d
Vadim Lisovyi <vadim.lisovyi@gmail.com>
2019-08-21 23:24:03 +0300

    [BOODMO-16973] sort orders

--- src/app/page/orders/orders.component.ts
+++ src/app/page/orders/orders.component.ts
@@ -3,6 +3,7 @@ import {
 } from '@angular/core';
 import { ModelService } from '../../model/model.service';
 import { Order } from '../../model/entity/order/order';
+import { InvoiceItems } from '../../model/aggregate/invoice-items';
 import { VirtualDateService } from '../../util/virtual-date.service';
 
 @Component({
@@ -10,8 +11,8 @@ import { VirtualDateService } from '../../util/virtual-date.service';
   templateUrl: 'orders.component.html',
 })
 export class OrdersComponent {
-  order = 'createdAt';
-  reverse = false;
+  orderType = 'createdAt';
+  orderReverse = false;
 
   constructor(
     public model: ModelService,
@@ -23,11 +24,18 @@ export class OrdersComponent {
     return this.model.orderActions(order, this.dateService.getDate());
   }
 
-  setOrder(value: string) {
-    if (this.order === value) {
-      this.reverse = !this.reverse;
-    }
-
-    this.order = value;
+  getSortedGroup(order: Order): InvoiceItems[] {
+    return order.groupByInvoiceItems
+      .sort((a, b) => {
+        if (a.invoice && a.invoice.createdAt && b.invoice && b.invoice.createdAt) {
+          return this.orderReverse ? b.invoice.createdAt.getTime() - a.invoice.createdAt.getTime() :
+            a.invoice.createdAt.getTime() - b.invoice.createdAt.getTime();
+        } else {
+          if ((a.invoice && !a.invoice.createdAt && b.invoice && !b.invoice.createdAt) || (!a.invoice && !b.invoice)) {
+            return 0;
+          }
+          return this.orderReverse && b.invoice && b.invoice.createdAt ? -1 : 1;
+        }
+      });
   }
 }
